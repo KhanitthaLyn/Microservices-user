b1cbec51ccd795345d9fd4022933a78b3f98ab7e
KhanitthaLyn <khanitthamfu@gmail.com>
2025-12-06 14:00:04 +0700

    added cartItem

--- src/main/java/com/micro/ecom_micro/models/CartItem.java
+++ src/main/java/com/micro/ecom_micro/models/CartItem.java
@@ -1,4 +1,33 @@
 package com.micro.ecom_micro.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Integer quantity;
+    private BigDecimal price;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
